15a4cab26530f43568a729e1c88316916e12f9b8
anstlgus02 <chloe.moon@mail.utoronto.ca>
2023-10-30 12:23:05 -0400

    Created SignupState

--- src/interface_adapter/signup/SignupState.java
+++ src/interface_adapter/signup/SignupState.java
@@ -1,4 +1,35 @@
 package interface_adapter.signup;
 
 public class SignupState {
+    private String username = "";
+
+    private String password = "";
+    private String repeatPassword = "";
+
+    public SignupState(SignupState copy) {
+        username = copy.username;
+        password = copy.password;
+        repeatPassword = copy.repeatPassword;
+    }
+
+    public SignupState() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    @Override
+    public String toString(){
+        return "SignupState{" + "username='" + username + '\'' +
+                ", password='" + password + '\'' + ", repeatPassword='" + repeatPassword + '\'' + '}';
+    }
 }

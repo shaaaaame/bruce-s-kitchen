1fff8d0964dca917c6cb55925b2de5f329a65f84
duckquakes <jasmine.cheng@mail.utoronto.ca>
2023-11-01 16:07:53 -0400

    "MAIN"

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,17 @@
 package app;
+import entity.UserFactory;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.login.LoginUserDataAccessInterface;
+import view.LoggedInView;
+import view.LoginView;
+import view.SignupView;
+import view.ViewManager;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {

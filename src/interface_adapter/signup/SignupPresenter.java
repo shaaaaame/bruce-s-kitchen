903be655a1e5e9290609a01e4689bf2a56934cd9
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2023-11-01 16:54:05 -0400

    Completed signup presenter

--- src/interface_adapter/signup/SignupPresenter.java
+++ src/interface_adapter/signup/SignupPresenter.java
@@ -1,4 +1,36 @@
 package interface_adapter.signup;
 
-public class SignupPresenter {
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.signup.SignupOutputBoundary;
+import use_case.signup.SignupOutputData;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SignupPresenter implements SignupOutputBoundary {
+
+    private final SignupViewModel signupViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel,
+                           SignupViewModel signupViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.signupViewModel = signupViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData response) {
+        viewManagerModel.firePropertyChanged();
+        System.out.println("Success! :D");
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        SignupState signupState = signupViewModel.getState();
+        signupViewModel.firePropertyChanged();
+        System.out.println("Fail!!!!! :(");
+
+    }
 }

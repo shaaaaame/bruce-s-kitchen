1c58dfa43f77f5cfb58ac75d31cfcc57be6a3309
Michael <michaelhanwen.wu@mail.utoronto.ca>
2023-12-03 17:50:42 -0500

    So i can rebase

--- src/use_case/APIpull/RecipeOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package use_case.APIpull;
-
-public interface RecipeOutputBoundary {
-    public void prepareSuccessView(RecipeOutputData recipeOutputData);
-    public void prepareFailView(String error);
-}
--- src/use_case/APIpull/RecipeSearchInputBoundary.java
+++
@@ -1,5 +0,0 @@
-package use_case.APIpull;
-
-public interface RecipeSearchInputBoundary {
-    public void execute(RecipeSearchInputData recipeInputData);
-}

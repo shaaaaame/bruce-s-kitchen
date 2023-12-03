e724a733e44ede42411b6f84cd9ef807b3b70e05
Michael <michaelhanwen.wu@mail.utoronto.ca>
2023-12-02 22:11:15 -0500

    Made Recipe Factory

---
+++ src/entity/RecipeFactory.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+
+public class RecipeFactory {
+    public Recipe create(UUID recipe_id, UUID user_id, String name, String servings, List<String> ingredients, Tag[] tags, String instructions, LocalDateTime dateCreated){
+        return new Recipe(recipe_id, user_id, name, servings, ingredients, tags, instructions, dateCreated);
+    }
+
+    public Recipe create(UUID user_id, String name, String servings, List<String> ingredients, Tag[] tags, String instructions, LocalDateTime dateCreated){
+        return new Recipe(user_id, name, servings, ingredients, tags, instructions, dateCreated);
+    }
+}

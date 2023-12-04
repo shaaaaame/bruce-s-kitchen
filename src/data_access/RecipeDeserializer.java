b98d58971b28ee0680159004442f9c41907d5015
Michael <michaelhanwen.wu@mail.utoronto.ca>
2023-12-03 20:37:10 -0500

    Recipe Search interface adapter

---
+++ src/data_access/RecipeDeserializer.java
@@ -0,0 +1,85 @@
+package data_access;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import entity.Recipe;
+import entity.Tag;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class RecipeDeserializer extends StdDeserializer<List<Recipe>> {
+    public RecipeDeserializer(){
+        this(null);
+    }
+    public RecipeDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+
+    @Override
+    public List<Recipe> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        List<Recipe> recipeLists = new ArrayList<Recipe>();
+        JsonNode root = jsonParser.getCodec().readTree(jsonParser);
+
+
+        if(root.isArray()){
+            for(JsonNode node: root){
+
+                UUID recipeId = null;
+                UUID userId = null;
+
+                JsonNode recipeIdNode = node.get("recipeId");
+                if (recipeIdNode != null) {
+                    recipeId = UUID.fromString(recipeIdNode.asText());
+                }
+
+                JsonNode userIdNode = node.get("userId");
+                if (userIdNode != null && !userIdNode.isNull()) {
+                    userId = UUID.fromString(userIdNode.asText());
+                }
+
+
+                String name = node.get("title").asText();
+                String servings = node.get("servings").asText();
+
+                LocalDateTime dateCreated = LocalDateTime.now();
+                JsonNode timeIdNode = node.get("recipeId");
+                if (timeIdNode != null) {
+                    dateCreated = LocalDateTime.parse(node.get("dateCreated").asText());
+                }
+
+                Tag[] tags;
+                JsonNode tagNode = node.get("tags");
+                if (tagNode != null) {
+                    tags = new Tag[tagNode.size()];
+                    int i = 0;
+                    for(JsonNode branch: tagNode) {
+                        tags[i] = (new Tag(branch.asText()));
+                        i++;
+                    }
+                }
+                else {
+                    tags = null; //change
+                }
+
+
+                String instructions = node.get("instructions").asText();
+
+                List<String> ingredients = new ArrayList<>();
+                JsonNode ingredientNode = node.get("ingredients");
+                for (JsonNode ingredientBranch: ingredientNode){
+                    ingredients.add(ingredientBranch.asText());
+                }
+                recipeLists.add(new Recipe(recipeId, userId, name, servings, ingredients, tags, instructions, dateCreated));
+            }
+
+        }
+
+        return recipeLists;
+    }
+}
---
+++ src/use_case/recipe_search/RecipeSearchInteractor.java
@@ -0,0 +1,35 @@
+package use_case.recipe_search;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import data_access.InMemoryRecipeAPIDataAccessObject;
+import entity.Recipe;
+import entity.RecipeFactory;
+
+import java.util.List;
+
+public class RecipeSearchInteractor implements RecipeSearchInputBoundary{
+
+    final RecipeSearchDataAccessInterface recipeSearchDataAccessObject;
+    final RecipeFactory recipeSearchFactory;
+    final RecipeSearchOutputBoundary recipePresenter;
+    final InMemoryRecipeAPIDataAccessObject recipeSearchObject;
+    public RecipeSearchInteractor(RecipeSearchDataAccessInterface recipeSearchDataAccessObject, RecipeFactory recipeSearchFactory, RecipeSearchOutputBoundary recipePresenter, InMemoryRecipeAPIDataAccessObject recipeSearchObject) {
+        this.recipeSearchDataAccessObject = recipeSearchDataAccessObject;
+        this.recipeSearchFactory = recipeSearchFactory;
+        this.recipePresenter = recipePresenter;
+        this.recipeSearchObject = recipeSearchObject;
+    }
+
+    @Override
+    public void execute(RecipeSearchInputData recipeInputData) throws JsonProcessingException {
+
+        List<Recipe> recipeList = this.recipeSearchObject.searchRecipe(recipeInputData.getSearch());
+        RecipeSearchOutputData recipeOutputData;
+        if (recipeList.isEmpty()) {
+            recipeOutputData = new RecipeSearchOutputData(recipeList, true);
+        } else {
+            recipeOutputData = new RecipeSearchOutputData(recipeList, false);
+        }
+        recipePresenter.prepareSuccessView(recipeOutputData);
+    }
+}

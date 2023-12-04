72d7d02a615d6b24eddfc459fe862ef47a91ee3e
shaaaaame <hanxheng@gmail.com>
2023-12-04 02:06:52 -0500

    Added search view result functionality

--- src/interface_adapter/recipe_search/RecipeSearchState.java
+++ src/interface_adapter/recipe_search/RecipeSearchState.java
@@ -1,13 +1,22 @@
 package interface_adapter.recipe_search;
 
+import entity.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecipeSearchState {
     private String search = "";
+    private List<Recipe> recipeList = new ArrayList<Recipe>();
     public RecipeSearchState(){}
 
     public String getSearch(){return this.search;}
 
+    public List<Recipe> getRecipeList(){ return this.recipeList; }
+
     public void setSearch(String search){
         this.search = search;
     }
+    public void setRecipeList(List<Recipe> recipeList) { this.recipeList = recipeList; }
 
 }
--- src/interface_adapter/recipe_search/RecipeSearchViewModel.java
+++ src/interface_adapter/recipe_search/RecipeSearchViewModel.java
@@ -2,15 +2,31 @@ package interface_adapter.recipe_search;
 
 import interface_adapter.ViewModel;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class RecipeSearchViewModel extends ViewModel {
     public static final String TITLE_LABEL = "Search a recipe!";
 
     private RecipeSearchState state = new RecipeSearchState();
 
     public void setState(RecipeSearchState state) {this.state = state;}
-    public RecipeSearchState getState() {return state;}
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the Signup Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 
     public RecipeSearchViewModel(){
         super("Recipe Search");
     }
+
+    public RecipeSearchState getState() {return state;}
+
 }
